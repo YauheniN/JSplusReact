5a57004e8e4160a29b54d6d1767f9b5d85b4dbbc
Yauheni Niahanau <yauheninaby@gmail.com>
2021-09-06 23:49:12 +0300

    Функции перенесены в методы объекта personalMovieDB

--- js/script.js
+++ js/script.js
@@ -1,63 +1,63 @@
-let numberOfFilms;
-let markMovie, oneLastMovie;
+
 let personalMovieDB = {
-    count : numberOfFilms,
+    count : 0,
     movies : {},
     actors : {},
     genres : [],
-    privat : false
-}
-
-function start() {
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
-    }
-    personalMovieDB["count"] = numberOfFilms;    
-}
-
-function rememberMyFilms() {
-    for (i = 0; i < 2; i++) {
-        do {
-            oneLastMovie = prompt(`Один из последних просмотренных фильмов?`, '');
-            markMovie = prompt('На сколько Вы его оцените?', '');
+    privat : true,
+    start : function () {
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов Вы уже посмотрели?', '');
         }
-        while (oneLastMovie == null || oneLastMovie == '' || oneLastMovie.length > 50 || markMovie == null || markMovie == '');
-        personalMovieDB.movies[`${oneLastMovie}`] = markMovie;
-    }
-}
-
-function detectPersonalLevel() {
-    if (personalMovieDB['count'] >=0 && personalMovieDB['count'] < 10) {
-        alert('Просмотрено довольно мало фильмов');}
-    else if (personalMovieDB['count'] >= 10 && personalMovieDB['count'] < 30) {
-        alert('Вы классический зритель');}
-    else if (personalMovieDB['count'] >=30) {
-        alert('Вы киноман');
-    }
-    else {
-        alert('Произошла ошибка');
-    } 
-}
-
-function showMyDB() {
-    if (!personalMovieDB.privat) {
-        console.log(personalMovieDB);
-    }
-}
-
-function writeYourGenres() {
-    for(let i = 1; i<=3; i++) {
-        let oneBestGenres;
-        do {
-            oneBestGenres = prompt(`Ваш любимый жанр под номером ${i}?`, '');
+    },
+    rememberMyFilms : function () {
+        for (i = 0; i < 2; i++) {
+            let markMovie, oneLastMovie;
+            do {
+                oneLastMovie = prompt(`Один из последних просмотренных фильмов?`, '');
+                markMovie = prompt('На сколько Вы его оцените?', '');
+            }
+            while (oneLastMovie == null || oneLastMovie == '' || oneLastMovie.length > 50 || markMovie == null || markMovie == '');
+            personalMovieDB.movies[`${oneLastMovie}`] = markMovie;
         }
-        while (oneBestGenres == null || oneBestGenres == '' || oneBestGenres > 50);
-        personalMovieDB.genres.push(oneBestGenres);
+    },
+    detectPersonalLevel : function () {
+        if (personalMovieDB['count'] >=0 && personalMovieDB['count'] < 10) {
+            alert('Просмотрено довольно мало фильмов');}
+        else if (personalMovieDB['count'] >= 10 && personalMovieDB['count'] < 30) {
+            alert('Вы классический зритель');}
+        else if (personalMovieDB['count'] >=30) {
+            alert('Вы киноман');
+        }
+        else {
+            alert('Произошла ошибка');
+        } 
+    },
+    showMyDB : function () {
+        if (!personalMovieDB.privat) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres : function () {
+        for(let i = 1; i<=3; i++) {
+            let oneBestGenres;
+            do {
+                oneBestGenres = prompt(`Ваш любимый жанр под номером ${i}?`, '');
+            }
+            while (oneBestGenres == null || oneBestGenres == '' || oneBestGenres > 50);
+            personalMovieDB.genres.push(oneBestGenres);
+        }
+        personalMovieDB["genres"].forEach((item, index) => {
+            console.log(`Любимый жанр №${index + 1} - это ${item}`);
+        })
+    },
+    toggleVisibleMyDB : function () {
+        if (personalMovieDB.privat) {
+            personalMovieDB["privat"] = false;
+        } else {
+            personalMovieDB["privat"] = true;
+        }
     }
 }
 
-start();
-rememberMyFilms();
-detectPersonalLevel();
-writeYourGenres();
-showMyDB();
+
