23ac349d082b91ef79386bddd23fc4c16f4b90cc
Yauheni Niahanau <yauheninaby@gmail.com>
2021-09-19 20:39:47 +0300

    Добавлена авто подстановка классов элемента

--- dist/js/script.js
+++ dist/js/script.js
@@ -140,4 +140,76 @@ window.addEventListener('DOMContentLoaded', () =>{
     }
 
     window.addEventListener('scroll', showModalByScroll);
+
+    //startClass
+
+    class MenuCard {
+        constructor (src, alt, title, desc, price, selectorParent, ...classes) {
+            this.src  = src;
+            this.alt = alt;
+            this.title = title;
+            this.desc = desc;
+            this.price = price;
+            this.selectorParent = document.querySelector(selectorParent);
+            this.transfer = 27;
+            this.classes = classes;
+            this.changeCurrency();
+        }
+
+        changeCurrency() {
+            this.price = this.price * this.transfer;
+        }  
+
+        render() {
+            const element = document.createElement('div');
+
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(elementClass => element.classList.add(elementClass));
+            }
+
+            element.innerHTML = `
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.desc}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `;
+            this.selectorParent.append(element);
+        }
+    }
+
+    new MenuCard(
+        'img/tabs/vegy.jpg',
+        'vegy',
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+        ).render();
+
+    new MenuCard(
+        'img/tabs/elite.jpg',
+        'elite',
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        14,
+        '.menu .container',
+        'menu__item'
+        ).render();        
+
+    new MenuCard(
+        'img/tabs/post.jpg',
+        'post',
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        21,
+        '.menu .container',
+        'menu__item'
+        ).render();        
 })
